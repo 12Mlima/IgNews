fe9c3e32b7b7b6db101574326e4106623f97dbca
12Mlima <marsellima@gmail.com>
2022-02-24 21:11:57 -0300

    auth github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+// Temos 3 formas de fazer uma chamada API
+// CLient-side
+// Server-side
+// Static-side generation
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,12 +33,31 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>$ 9.90 month</span>
+            <span>for {product?.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KGvYDDSPYlfIRgE3oVxqt6u', {
+    expand: ['product'],
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 24, //24 horas
+  }
 }
