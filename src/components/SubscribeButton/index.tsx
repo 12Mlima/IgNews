aa56edf473bb297f6912c2d93f9871823d4cb370
12Mlima <marsellima@gmail.com>
2022-03-03 21:02:32 -0300

    finalização configuração stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -8,13 +11,33 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession()
 
-  function handleSubscribe() {
+  const router = useRouter()
+
+  async function handleSubscribe() {
     if (!session) {
-      signIn('gitgub')
+      signIn('github')
       return
     }
-  }
 
+    if (session.activeSubscription) {
+      router.push('/posts')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({
+        sessionId,
+      })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
   return (
     <button
       type="button"
